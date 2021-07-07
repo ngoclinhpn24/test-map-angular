054d738a0f265f7ba3003810187ef6509582255c
Linh Pham <linhpn2402@gmail.com>
2021-07-07 17:25:58 +0700

    map2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 7.809007;
+  lat = 9.8372633;
+  lng = 105.5193633;
+
+  zoom = 9;
+  
+  lat2 = 9.8368873;
+  lng2 = 105.5169083;
 
   onChoseLocation(event: any){
     console.log(event);
     // this.lat = event.coords.lat;
     // this.lng = event.coords.lng;
   }
+
+  markers = [
+    {
+      lat: 21.1594627,
+      lng: 72.6822083,
+      label: 'Surat' },
+    
+    { 
+      lat: 23.0204978,
+      lng: 72.4396548,
+      label: 'Ahmedabad'},
+
+    {
+      lat: 22.2736308,
+      lng: 70.7512555,
+      label: 'Rajkot'}  ];
 }
